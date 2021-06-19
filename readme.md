ed5f1c5ee92212aebe885e72eb9596b66125c154
as60160 <as60160@users.noreply.github.com>
2021-06-20 06:17:20 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+# JS - To do list
+
+## Demo
+- [Demo]()
+
+## 來源
+- 六角學院 2021 程式體驗營
+- 切版任務 05
+- [設計稿](https://hackmd.io/J7ajdobzTlyideAARTLz5Q?view)
+
+## 使用技術/框架
+- Sass
+- JavaScript、ES6
+
+## 實作功能
+- 新增事項
+- 刪除事項
+- 切換狀態 (完成/未完成)
+- 統計數量 (未完成事項)
+- 篩選資料 (全部/待完成/已完成)
+- 清除所有已完成項目
+
+## 第三方套件
+- Font Awesome
+- Google Fonts
---
+++ script.js
@@ -0,0 +1,114 @@
+const text = document.querySelector(".textInput")
+const addBtn = document.querySelector(".addBtn")
+const nav = document.querySelector(".nav")
+const list = document.querySelector(".list")
+const undoneNumber = document.querySelector(".undoneNumber")
+const clearBtn = document.querySelector(".clearBtn")
+const data = []
+
+addBtn.addEventListener("click", addItem)
+list.addEventListener("click", deleteItem)
+list.addEventListener("click", changeItemState)
+nav.addEventListener("click", changeCategory)
+clearBtn.addEventListener("click", cleanDoneItems)
+
+renderData()
+
+function renderData(items) {
+  let newList = []
+  let str = ""
+  if (data.length !== 0) {
+    document.querySelector(".listWrap").style.display = "block"
+  }
+
+  if (items !== undefined) {
+    newList = items
+  } else {
+    newList = data
+  }
+
+  newList.forEach((item, index) => {
+    str += `
+      <li>
+        <a href="#" class=${item.isDone? "done":"undone"}></a>
+        <p>${item.content}</p>
+        <a href="#" class="deleteBtn" data-num="${index}">
+          <img src="img/cancel.jpg"></img>
+        </a>
+      </li>
+    `
+  })
+
+  list.innerHTML = str
+  updateUndoneNumber()
+}
+
+function updateUndoneNumber() {
+  let undone = data.filter(item => item.isDone == false)
+  undoneNumber.textContent = undone.length
+}
+
+function addItem() {
+  if (!text.value) {
+    alert("請輸入待辦事項")
+    return
+  }
+  let newItem = {
+    content: text.value.trim(),
+    isDone: false
+  }
+  data.push(newItem)
+  renderData(data)
+  text.value = ""
+}
+
+function deleteItem(e) {
+  if (e.target.nodeName == "IMG") {
+    let num = e.target.parentNode.getAttribute("data-num")
+    data.splice(num, 1)
+    renderData()
+  }
+}
+
+function changeItemState(e) {
+  if (e.target.nodeName == "A" || e.target.nodeName == "P") {
+    let num = e.target.parentNode.childNodes[5].getAttribute("data-num")
+    data[num].isDone = !(data[num].isDone)
+    renderData()
+  }
+}
+
+function changeCategory(e) {
+  const catagories = document.querySelectorAll("[data-selected]")
+  const activeItem = e.target.getAttribute("data-selected")
+  catagories.forEach(item => {
+    if (item.getAttribute("data-selected") == activeItem) {
+      item.classList.add("active")
+    } else {
+      item.classList.remove("active")
+    }
+  })
+  filterData(activeItem)
+}
+
+function filterData(catagories) {
+  let items
+  switch (catagories) {
+    case "done":
+      items = data.filter(item =>  item.isDone == true)
+      break
+    case "undone":
+      items = data.filter(item => item.isDone == false)
+      break
+    default:
+      items = data
+  }
+  renderData(items)
+}
+
+function cleanDoneItems() {
+  for (let i = data.length - 1; i >= 0; i--){
+    if (data[i].isDone == true) data.splice(i,1)
+  }
+  renderData()
+}
