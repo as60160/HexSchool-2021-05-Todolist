5d120afc4f081001830634c0df2328b70ad16c9a
as60160 <as60160@users.noreply.github.com>
2021-08-18 02:10:51 +0800

    reset id, rebuild filter logic and add hint messages

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 const text = document.querySelector(".textInput")
 const addBtn = document.querySelector(".addBtn")
 const nav = document.querySelector(".nav")
+const catagories = document.querySelectorAll("[data-selected]")
 const list = document.querySelector(".list")
 const undoneNumber = document.querySelector(".undoneNumber")
 const clearBtn = document.querySelector(".clearBtn")
-const data = []
+const msg = ["哎呀，您還沒輸入待辦事項喔！", "咦，怎麼空白？再試一次！", "嗨~您是不是忘了打字？？", "要做的事可能被我吃了，不然您再輸入一次？"]
+
+let data = []
+let category = "all"
 
 addBtn.addEventListener("click", addItem)
 text.addEventListener("keyup", detectKey)
 list.addEventListener("click", deleteItem)
 list.addEventListener("click", changeItemState)
-nav.addEventListener("click", changeCategory)
+nav.addEventListener("click", detectCategory)
 clearBtn.addEventListener("click", cleanDoneItems)
 
 renderData()
 
-function renderData(items) {
+function renderData() {
   let newList
   let str = ""
+
   if (data.length !== 0) {
     document.querySelector(".listWrap").style.display = "block"
   }
 
-  if (items !== undefined) {
-    newList = items
-  } else {
-    newList = data
+  switch (category) {
+    case "done":
+      newList = data.filter(item =>  item.isDone == true)
+      break
+    case "undone":
+      newList = data.filter(item => item.isDone == false)
+      break
+    default:
+      newList = data
   }
 
-  newList.forEach((item, index) => {
+  newList.forEach((item) => {
     str += `
       <li>
         <a href="#" class=${item.isDone? "done":"undone"}></a>
         <p>${item.content}</p>
-        <a href="#" class="deleteBtn" data-num="${index}">
+        <a href="#" class="deleteBtn" data-id="${item.id}">
           <img src="img/cancel.jpg"></img>
         </a>
       </li>
@@ -55,22 +65,26 @@ function detectKey(e){
 }
 
 function addItem() {
-  if (!text.value) {
-    alert("請輸入待辦事項")
+  if (!text.value.trim()) {
+    const num = Math.floor(Math.random()*msg.length)
+    text.setAttribute("placeholder", msg[num])
     return
   }
   const newItem = {
     content: text.value.trim(),
-    isDone: false
+    isDone: false,
+    id: Date.now()
   }
   data.push(newItem)
-  renderData(data)
+  renderData()
   text.value = ""
+  text.setAttribute("placeholder", "新增待辦事項")
 }
 
 function deleteItem(e) {
   if (e.target.nodeName == "IMG") {
-    const num = e.target.parentNode.getAttribute("data-num")
+    const id = e.target.parentNode.getAttribute("data-id")
+    const num = data.findIndex(item => item.id == id)
     data.splice(num, 1)
     renderData()
   }
@@ -78,43 +92,34 @@ function deleteItem(e) {
 
 function changeItemState(e) {
   if (e.target.nodeName == "A" || e.target.nodeName == "P") {
-    let num = e.target.parentNode.childNodes[5].getAttribute("data-num")
+    const id = e.target.parentNode.childNodes[5].getAttribute("data-id")
+    const num = data.findIndex(item => item.id == id)
     data[num].isDone = !(data[num].isDone)
-    renderData()
   }
+
+  const undoneNum = data.filter(item => item.isDone === false).length
+  if (undoneNum === 0 || undoneNum === data.length) category = "all"
+  changeCategory()
 }
 
-function changeCategory(e) {
-  const catagories = document.querySelectorAll("[data-selected]")
-  const activeItem = e.target.getAttribute("data-selected")
+function detectCategory(e) {
+  category = e.target.getAttribute("data-selected")
+  changeCategory()
+}
+
+function changeCategory() {
   catagories.forEach(item => {
-    if (item.getAttribute("data-selected") == activeItem) {
+    if (item.getAttribute("data-selected") == category) {
       item.classList.add("active")
     } else {
       item.classList.remove("active")
     }
   })
-  filterData(activeItem)
-}
-
-function filterData(catagories) {
-  let items
-  switch (catagories) {
-    case "done":
-      items = data.filter(item =>  item.isDone == true)
-      break
-    case "undone":
-      items = data.filter(item => item.isDone == false)
-      break
-    default:
-      items = data
-  }
-  renderData(items)
+  renderData()
 }
 
 function cleanDoneItems() {
-  for (let i = data.length - 1; i >= 0; i--){
-    if (data[i].isDone == true) data.splice(i,1)
-  }
-  renderData()
+  data = data.filter(item => item.isDone === false)
+  category = "all"
+  changeCategory()
 }
